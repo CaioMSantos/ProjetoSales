767a0a333a41aaac6c4bc0ca72e4d5f8dfe16883
Caio Marcio <caioms321@gmail.com>
2021-02-18 11:03:04 -0300

    Primeiro teste MVC: Department

--- ProjetoSalesMVC/Controllers/HomeController.cs
+++ ProjetoSalesMVC/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ProjetoSalesMVC.Models;
+using ProjetoSalesMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;

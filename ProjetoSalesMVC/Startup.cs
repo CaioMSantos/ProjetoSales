633b69b2c5352268df04f1e85e6212bc2a2e293d
Caio Marcio <caioms321@gmail.com>
2021-02-18 11:18:31 -0300

    CRUD Scaffolding

--- ProjetoSalesMVC/Startup.cs
+++ ProjetoSalesMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSalesMVC.Data;
 
 namespace ProjetoSalesMVC
 {
@@ -33,6 +35,9 @@ namespace ProjetoSalesMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoSalesMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoSalesMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
